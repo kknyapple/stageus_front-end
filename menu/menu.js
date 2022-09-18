31dfad6b6e6d071bd428ae544172c93c39bac399
kknyapple <72698829+kknyapple@users.noreply.github.com>
2022-09-19 01:28:37 +0900

    mouseover, mouseleave로 메뉴 이벤트 주기

---
+++ menu/menu.js
@@ -0,0 +1,23 @@
+const createMenu = function (svg, name, id) {
+  const menus = document.getElementById("menus");
+  const menuId = document.createElement("div");
+  const menu = document.createElement("div");
+  const menuButton = document.createElement("button");
+  const menuSvg = document.createElement("div");
+  const menuName = document.createElement("p");
+
+  menuId.setAttribute("id", `menu${id}`);
+  menu.setAttribute("id", "menu");
+  menuButton.setAttribute("id", "button-menu");
+
+  menuSvg.innerHTML = `${svg}`;
+  menuName.innerHTML = `${name}`;
+
+  menus.appendChild(menuId);
+  menuId.appendChild(menu);
+  menu.appendChild(menuButton);
+  menuButton.appendChild(menuSvg);
+  menuButton.appendChild(menuName);
+};
+
+export default createMenu;
