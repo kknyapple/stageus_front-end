b8ca01aa48c04cc05cf14d9ac68673328b02339a
kknyapple <72698829+kknyapple@users.noreply.github.com>
2022-10-27 17:10:07 +0900

    Delete src directory

--- src/menu/menuHover.js
+++
@@ -1,9 +0,0 @@
-const hoveringMenu = function (id) {
-  const menu = document.getElementById(`menu${id}`);
-  menu.addEventListener("mouseover", () => {
-    menu.style.backgroundColor = "lightgray";
-  });
-  menu.addEventListener("mouseleave", () => {
-    menu.style.backgroundColor = "transparent";
-  });
-};
