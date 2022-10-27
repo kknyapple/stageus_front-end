84c152433fd086be492f2874ce22cf707ae8db2b
kknyapple <72698829+kknyapple@users.noreply.github.com>
2022-10-27 16:53:14 +0900

    video click event 수정

--- youtube_redux/src/component/Container/Video/VideoComponent.js
+++ youtube_redux/src/component/Container/Video/VideoComponent.js
@@ -36,34 +36,6 @@ export default function VideoComponent() {
     ],
   };
 
-  const [videoScale, setVideoScale] = React.useState(false);
-
-  const videoOnClickHandler = (e) => {
-    const eventTarget = e.target.parentNode.parentNode;
-    console.log(videoScale);
-    setVideoScale(!videoScale);
-    if (eventTarget.id == "video") {
-      if (videoScale === true) {
-        eventTarget.parentNode.style.transform = "scale(1.1)";
-        eventTarget.parentNode.style.top = "-10px";
-      } else if (videoScale === false) {
-        eventTarget.parentNode.style.top = "0px";
-        eventTarget.parentNode.style.transform = "scale(1)";
-      }
-    }
-  };
-
-  React.useEffect(() => {
-    document
-      .getElementById("videos")
-      .addEventListener("click", videoOnClickHandler);
-    // return () => {
-    //   document
-    //     .getElementById("videos")
-    //     .removeEventListener("click", videoOnClickHandler);
-    // };
-  }, []);
-
   return (
     <div id="videos">
       {videoInfoObj.id.map((a, index) => {
--- youtube_redux/src/component/Container/Video/VideoItemComponent.js
+++ youtube_redux/src/component/Container/Video/VideoItemComponent.js
@@ -1,12 +1,37 @@
 import React from "react";
+import { useState } from "react";
+
+const useVideo = () => {
+  const [videoScale, setVideoScale] = useState(true);
+
+  const videoCondition = (scale) => {
+    setVideoScale(!scale);
+  };
+
+  return [videoScale, videoCondition];
+};
 
 export default function VideoItemComponent(props) {
   const id = props.id;
   const img = props.img;
   const content = props.content;
+  // const [videoState, setVideoState] = useVideo();
+  const [videoState, setVideoState] = useState(true);
+
+  const videoOnClickHandler = (e) => {
+    const eventTarget = e.currentTarget;
+    setVideoState(!videoState);
+    if (videoState === true) {
+      eventTarget.style.transform = "scale(1.1)";
+      eventTarget.style.top = "-10px";
+    } else if (videoState === false) {
+      eventTarget.style.top = "0px";
+      eventTarget.style.transform = "scale(1)";
+    }
+  };
 
   return (
-    <div id={`video${props.index}`}>
+    <div id={`video${props.index}`} onClick={videoOnClickHandler}>
       <div id="video">
         <div id="thumbnail">
           <img src={`./images/${img[props.index]}.jpg`} />
