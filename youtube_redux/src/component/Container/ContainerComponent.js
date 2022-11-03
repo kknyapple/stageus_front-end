da10748b1b8465a154a2b82b627aadd408e778ac
kknyapple <72698829+kknyapple@users.noreply.github.com>
2022-11-03 19:39:30 +0900

    component 넣는 방식 수정

--- youtube_redux/src/component/Container/ContainerComponent.js
+++ youtube_redux/src/component/Container/ContainerComponent.js
@@ -10,7 +10,14 @@ export default function ContainerComponent() {
   return (
     <main id="container">
       <NavComponent />
-      {page == 0 ? <VideoComponent /> : <div>{`page${page}`}</div>}
+      {(page == 0 && <VideoComponent />) ||
+        (page == 1 && <div>Component1</div>) ||
+        (page == 2 && <div>Component2</div>) ||
+        (page == 3 && <div>Component3</div>) ||
+        (page == 4 && <div>Component4</div>) ||
+        (page == 5 && <div>Component5</div>) ||
+        (page == 6 && <div>Component6</div>) ||
+        (page == 7 && <div>Component7</div>)}
     </main>
   );
 }
